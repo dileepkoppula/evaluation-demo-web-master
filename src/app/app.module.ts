d063e6b67ea45bfc605f3427db36d30598aedd8a
dileep koppula <nanikoppula9494@gmail.com>
2023-04-18 12:12:41 +0530

    dileep changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { appRoutingModule } from './app.routing';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AlertComponent } from './_components';
+import { AuditComponent } from './audit';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Ng2OrderModule } from 'ng2-order-pipe';
+import { SearchFilterPipe } from './search-filter.pipe';
+// import { NgxPaginationModule } from 'ngx-pagination';
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule,
+        FormsModule,
+        Ng2SearchPipeModule,
+        Ng2OrderModule,
+        // NgxPaginationModule,
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        AuditComponent,
+        AlertComponent,
+        SearchFilterPipe
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { };
---
+++ src/app/audit/audit.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from "@angular/core";
+import { first } from "rxjs/operators";
+
+import { Audit } from "@/_models";
+import { AuditService, AuthenticationService } from "@/_services";
+
+@Component({ templateUrl: "audit.component.html" })
+export class AuditComponent implements OnInit {
+  user: any;
+  audits = [];
+  p: number = 1;
+  key: string = "id";
+  reverse: boolean = false;
+  show12HrFormat: boolean = false;
+  constructor(
+    private authenticationService: AuthenticationService,
+    private auditService: AuditService
+  ) {}
+
+  ngOnInit() {
+    this.loadAllAudits();
+  }
+
+  private loadAllAudits() {
+    this.auditService
+      .getAll()
+      .pipe(first())
+      .subscribe((audits) => (this.audits = audits));
+  }
+
+  search() {
+    if (this.user == "") {
+      this.ngOnInit();
+    } else {
+      this.audits = this.audits.filter((res) => {
+        return res.user
+          .toLocaleLowerCase()
+          .match(this.user.toLocaleLowerCase());
+      });
+    }
+  }
+
+  
+  sort(key: string) {
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+
+  onChangeFormat(value){
+    console.log(value);
+    if(value=='12'){
+      this.show12HrFormat=true;
+    } else{
+      this.show12HrFormat=false;
+      
+    }
+  }
+}
